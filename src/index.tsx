825220cd8c708fb113a6150ddcaefb86dcc21df8
cecialbero <ceci.albero@gmail.com>
2022-04-18 14:26:09 -0300

    Creates display and margin helper classes

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,14 @@ import ReactDOM from "react-dom"
 import { App } from "./App"
 import reportWebVitals from "./reportWebVitals"
 import * as serviceWorker from "./serviceWorker"
+import CoinsState from './context/CoinsState'
+import './styles/styles.scss'
 
 ReactDOM.render(
   <React.StrictMode>
+  <CoinsState>
     <App />
+  </CoinsState>
   </React.StrictMode>,
   document.getElementById("root"),
 )
