fce3831b0007230b9ebb42d376d608c2b022501e
cecialbero <ceci.albero@gmail.com>
2022-04-19 12:04:35 -0300

    Move app heading inside of app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import { ChakraProvider, theme, Container } from '@chakra-ui/react';
+import { ChakraProvider, theme, Container, Heading } from '@chakra-ui/react';
 import CryptoTable from './components/CryptoTable';
 import Search from './components/Search';
+
+import './App.scss';
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Container maxW='900px'>
+      <Heading as='h1' size='lg'>Top 10 Cryptocurrency Prices by Market Cap</Heading>
       <Search />
       <CryptoTable />
     </Container>
--- src/components/CryptoTable.tsx
+++ src/components/CryptoTable.tsx
@@ -1,33 +1,31 @@
 import { useContext } from 'react'
-import { Table, Tbody, Thead, Tr, Th, TableContainer, Heading } from '@chakra-ui/react'
+import { Table, Tbody, Thead, Tr, Th, TableContainer } from '@chakra-ui/react'
 import { CoinsContext } from '../context/CoinsContext'
 import CoinRow from './CoinRow'
 
 const CryptoTable = () => {
     const coinsContext = useContext(CoinsContext);
-    const { coins } = coinsContext;
+    const { coins, loading } = coinsContext;
+    console.log(loading)
 
     return (
-        <>
-            <Heading></Heading>
-            <TableContainer>
-                <Table>
-                    <Thead>
-                        <Tr>
-                            <Th>#</Th>
-                            <Th>Coin</Th>
-                            <Th></Th>
-                            <Th isNumeric>Price</Th>
-                            <Th isNumeric>Mkt Cap</Th>
-                            <Th></Th>
-                        </Tr>
-                    </Thead>
-                    <Tbody>
-                        {coins.map((coin, index) => <CoinRow coin={coin} key={index}/>)}
-                    </Tbody>
-                </Table>
-            </TableContainer>
-        </>
+        <TableContainer>
+            <Table>
+                <Thead>
+                    <Tr>
+                        <Th>#</Th>
+                        <Th>Coin</Th>
+                        <Th></Th>
+                        <Th isNumeric>Price</Th>
+                        <Th isNumeric>Mkt Cap</Th>
+                        <Th></Th>
+                    </Tr>
+                </Thead>
+                <Tbody>
+                    {coins.map((coin, index) => <CoinRow coin={coin} key={index}/>)}
+                </Tbody>
+            </Table>
+        </TableContainer>
     )
 }
 
