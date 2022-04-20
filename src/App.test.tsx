e6a14f64618c30a28343cfa051404888a0f9f40b
cecialbero <ceci.albero@gmail.com>
2022-04-20 17:48:20 -0300

    Create error and title tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,25 @@
-import React from "react"
-import { screen } from "@testing-library/react"
-import { render } from "./test-utils"
-import { App } from "./App"
+import { screen, render } from '@testing-library/react'
+import { App } from './App'
+import { CoinsContext } from './context/CoinsContext'
 
-test("renders learn react link", () => {
+test('must display a title', () => {
   render(<App />)
-  const linkElement = screen.getByText(/learn chakra/i)
-  expect(linkElement).toBeInTheDocument()
+  const appTitle = screen.getByText(/Top 10 Cryptocurrency Prices by Market Cap/i)
+
+  expect(appTitle).toBeInTheDocument()
+})
+
+test('must display an error message if error is true', async () => {
+  render(<CoinsContext.Provider value={{
+    coins: [],
+    loading: false,
+    error: true,
+    getCoins(){}
+  }}>
+    <App />
+  </CoinsContext.Provider>)
+
+  expect(await screen.findByRole('alert', {name: ''})).toBeInTheDocument()
+  expect(await screen.findByText(/Ups, looks like there's no data available/i)).toBeInTheDocument()
+  expect(await screen.findByText(/We are working on a solution, try again later/i)).toBeInTheDocument()
 })
